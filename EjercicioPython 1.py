271111ddeebd56d189f50b15ca84b4ab71d83492
antonBlagodarnyy <104981469+antonBlagodarnyy@users.noreply.github.com>
2024-10-14 16:12:33 +0200

    Create EjercicioPython 1.py
    commit inicial

---
+++ EjercicioPython 1.py
@@ -0,0 +1,21 @@
+#Ej1
+nombre = input("Como se llama?")
+
+print (f'''¡Hola {nombre}! - Me alegro de conocerle, {nombre}''')
+
+#Ej2
+nombre = input("Como se llama?")
+
+print ("Nombre:"+nombre.upper())
+
+print ("Numero de ocurrencias:"+str(len(nombre)))
+
+for i in range (0, len(nombre)):
+    print(nombre)
+
+#Ej3
+num = input ("Introduzca un numero")
+num = int(num)
+for i in range (0, num):
+    if i % 2 == 0:
+        print(str(i))
